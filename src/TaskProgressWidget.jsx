d78d4de08d1038f014b38361322e49cfb4a6a6aa
arielmoguillansky <arielmogui92@gmail.com>
2023-11-27 20:00:46 -0300

    added icons, updated styles, accordion feature

--- src/TaskProgressWidget.jsx
+++ src/TaskProgressWidget.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import icon from "./icons/icon.svg";
+import completed from "./icons/icon-check.svg";
 import check from "./icons/check.svg";
+import arrow from "./icons/arrow.svg";
 import "./TaskProgressWidget.css";
 import { CheckboxComponent } from "./components/CheckboxComponent";
 
@@ -8,6 +10,7 @@ const TaskProgressWidget = () => {
   const [completedTasks, setCompletedTasks] = useState({});
   const [progressData, setProgressData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [expandedGroups, setExpandedGroups] = useState([]);
 
   useEffect(() => {
     // Fetch data from the API
@@ -23,8 +26,13 @@ const TaskProgressWidget = () => {
             (task) => task.checked || false
           );
         });
+        const parsedData = data.map((group) => ({
+          ...group,
+          completed: groupCompleted(group.tasks),
+        }));
+        groupCompleted();
         setCompletedTasks(initialCompletedTasks);
-        setProgressData(data);
+        setProgressData(parsedData);
         setLoading(false);
       })
       .catch((error) => {
@@ -40,12 +48,41 @@ const TaskProgressWidget = () => {
       const groupTasks = [...prevTasks[groupName]];
       // change value of task by its index
       groupTasks[taskIndex] = !groupTasks[taskIndex];
-
       return {
         ...prevTasks,
         [groupName]: groupTasks,
       };
     });
+
+    setProgressData((prevGroup) => {
+      const updatedGroup = prevGroup.map((g) => {
+        if (g.name === groupName) {
+          // Create a new array of tasks with the updated checked value
+          const updatedTasks = g.tasks.map((task, i) => ({
+            ...task,
+            checked: i === taskIndex ? !task.checked : task.checked,
+          }));
+
+          // Use the updatedTasks array to calculate the 'completed' property
+          const completed = groupCompleted(updatedTasks);
+
+          return { ...g, tasks: updatedTasks, completed };
+        }
+        return g;
+      });
+
+      return updatedGroup;
+    });
+  };
+
+  const groupCompleted = (tasks) => {
+    if (tasks) {
+      const totalPossibleValue = tasks.reduce(
+        (taskCount, task) => taskCount + task.checked,
+        0
+      );
+      return tasks.length === totalPossibleValue;
+    }
   };
 
   const calculateCompletion = () => {
@@ -74,6 +111,14 @@ const TaskProgressWidget = () => {
     return Math.round((completedValue / totalPossibleValue) * 100 || 0); // Prevent division by zero - if no tasks with values were given
   };
 
+  const toggleGroup = (groupName) => {
+    setExpandedGroups((prevGroups) =>
+      prevGroups.includes(groupName)
+        ? prevGroups.filter((group) => group !== groupName)
+        : [...prevGroups, groupName]
+    );
+  };
+
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -98,66 +143,88 @@ const TaskProgressWidget = () => {
         </div>
       </div>
 
-      {progressData.map((group) => (
-        <div className="accordeon" key={group.name}>
-          <div className="group-heading">
-            <img src={icon} alt="group icon" />
-            <h3>{group.name}</h3>
+      <div className="accordion">
+        {progressData.map((group) => (
+          <div className="accordion-row" key={group.name}>
+            <div
+              className="group-heading"
+              onClick={() => toggleGroup(group.name)}
+            >
+              {groupCompleted(group.tasks) ? (
+                <div className="heading-title">
+                  <img src={completed} alt="group icon" />
+                  <h3 style={{ color: "#00B797" }}>{group.name}</h3>
+                </div>
+              ) : (
+                <div className="heading-title">
+                  <img src={icon} alt="group icon" />
+                  <h3>{group.name}</h3>
+                </div>
+              )}
+              <div
+                className={`heading-action ${
+                  expandedGroups.includes(group.name) ? "active" : ""
+                }`}
+              >
+                {expandedGroups.includes(group.name) ? "Hide" : "Show"}
+                <img src={arrow} alt="arrow icon" />
+              </div>
+            </div>
+
+            {expandedGroups.includes(group.name) && (
+              <ul>
+                {group.tasks.map((task, index) => (
+                  <li key={index}>
+                    <label htmlFor={`${group.name}-${index}`}>
+                      {completedTasks[group.name][index] ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          viewBox="0 0 16 16"
+                          fill="none"
+                        >
+                          <rect width="16" height="16" rx="4" fill="#00B797" />
+                          <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M6.49574 10.1007L3.888 7.30201L3 8.24832L6.49574 12L13 4.94631L12.1182 4L6.49574 10.1007Z"
+                            fill="white"
+                          />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          viewBox="0 0 16 16"
+                          fill="none"
+                        >
+                          <rect
+                            x="0.5"
+                            y="0.5"
+                            width="15"
+                            height="15"
+                            rx="3.5"
+                            stroke="#999999"
+                          />
+                        </svg>
+                      )}
+                      <input
+                        type="checkbox"
+                        id={`${group.name}-${index}`}
+                        checked={completedTasks[group.name][index]}
+                        onChange={() => handleTaskToggle(group.name, index)}
+                      />
+                      <span>{task.description}</span>
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
-          <ul>
-            {group.tasks.map((task, index) => (
-              <li key={index}>
-                <label htmlFor={`${group.name}-${index}`}>
-                  {/* <CheckboxComponent
-                    isChecked={completedTasks[group.name][index]}
-                  /> */}
-                  {completedTasks[group.name][index] ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 16 16"
-                      fill="none"
-                    >
-                      <rect width="16" height="16" rx="4" fill="#00B797" />
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M6.49574 10.1007L3.888 7.30201L3 8.24832L6.49574 12L13 4.94631L12.1182 4L6.49574 10.1007Z"
-                        fill="white"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      viewBox="0 0 16 16"
-                      fill="none"
-                    >
-                      <rect
-                        x="0.5"
-                        y="0.5"
-                        width="15"
-                        height="15"
-                        rx="3.5"
-                        stroke="#999999"
-                      />
-                    </svg>
-                  )}
-                  <input
-                    type="checkbox"
-                    id={`${group.name}-${index}`}
-                    checked={completedTasks[group.name][index]}
-                    onChange={() => handleTaskToggle(group.name, index)}
-                  />
-                  {task.description}
-                </label>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
